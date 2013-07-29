e88f35a64b7cf73603431d846eaea5ba44d1732d
tlcine <mandolin74@cloak.gli.ph>
2013-07-29 12:13:59 +1000

    Update and rename website.pl to website.pm
    Ping main chat with current hub website address when requested

---
+++ commands/website.pm
@@ -0,0 +1,34 @@
+package website;
+
+use strict;
+use warnings;
+
+use FindBin;
+use lib "$FindBin::Bin/..";
+use DCBSettings;
+
+sub main {
+  my $command = shift;
+  my $user = shift;
+  my $chat = shift;
+  
+  my @return = ();
+  
+  my $hub_name = DCBSettings::config_get('hubname');
+  my $hub_href = DCBSettings::config_get('website');
+  my $message = "Link to " . $hub_name . " website: " . $hub_href;
+  
+  @return = (
+    {
+      param => "message",
+      message => $message,
+      type => 4,
+      user => '',
+      touser => '',
+    },
+  );
+  
+  return @return;
+}
+
+1;
