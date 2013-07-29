39c843ee98e062f19f909803e53a3559badf55f7
tlcine <mandolin74@cloak.gli.ph>
2013-07-29 22:01:45 +1000

    Add russianroulette.{pm,yml}
    Behaviour modified from previous. No longer reports ``remaining''
    unchecked barrels, barrels are fired in sequence after a random barrel
    is chosen following a successful shot.

---
+++ commands/russianroulette.pm
@@ -0,0 +1,58 @@
+package russianroulette;
+
+use strict;
+use warnings;
+
+use FindBin;
+use lib "$FindBin::Bin/..";
+use DCBSettings;
+
+sub main {
+  my $command = shift;
+  my $user = shift;
+
+  my @return = ();
+  my $num_barrels = 6;
+
+  my $barrel = DCBSettings::get_config('russianroulette_barrel');
+
+  if ($barrel == 0) {
+    $message = "BANG\!";
+    # Load a bullet and spin to a random barrel
+    $barrel = int(rand($num_barrels));
+    @return = (
+      {
+        param => "message",
+        message => $message,
+        user => '',
+        touser => '',
+        type => 4,
+      },
+      {
+        param => "action",
+        action => "kick",
+        arg => $message,
+        user => $user->{name},
+      },
+    );
+  } else {
+    $message = "Click\!";
+    $barrel--;
+    @return = (
+      {
+        param => "message",
+        message => $message,
+        user => '',
+        touser => '',
+        type => 4,
+      },
+    );
+  }
+
+  # Update current cocked barrel
+  DCBSettings::config_set('russianroulette_barrel', $barrel);
+
+  return @return;
+}
+
+1;
