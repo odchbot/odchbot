8dc20c0cd6f2bac056999499774b406ec765d38e
odchbot <odchbot@gmail.com>
2013-09-10 18:51:43 +1000

    Added in user welcoming to the bot so users and limited details are posted to the user on entry

--- commands/user.pm
+++ commands/user.pm
@@ -7,19 +7,19 @@ use FindBin;
 use lib "$FindBin::Bin/..";
 use DCBSettings;
 use DCBDatabase;
+use DCBCommon;
 use DCBUser;
-
-use Data::Dumper;
 
 sub postlogin {
   my $command = shift;
   my $user = shift;
   my @return = ();
+  my $hubname = DCBSettings::config_get('hubname');
   if ($user->{new}) {
     @return = (
       {
         param    => "message",
-        message  => "Welcome for the first time: $user->{name}",
+        message  => "Welcome to $hubname for the first time: $user->{name}",
         user     => '',
         fromuser   => '',
         type     => 4,
@@ -30,13 +30,42 @@ sub postlogin {
     @return = (
       {
         param    => "message",
-        message  => "Welcome back $user->{name}",
+        message  => "Welcome back to $hubname $user->{name}",
         user     => $user->{name},
         fromuser   => '',
         type     => 2,
       },
     );
   }
+  # Provide the user with additional welcome information
+  my $permissions = DCBUser::PERMISSIONS;
+  my %perm = %{$permissions};
+  my $perm = 'UNKNOWN';
+  foreach my $val (keys %perm) {
+    if ($perm{$val} == $user->{permission}) {
+      $perm = $val;
+    }
+  }
+
+  my $member_time = DCBCommon::common_timestamp_duration($user->{'join_time'});
+  my $share_delta = DCBCommon::common_format_size($user->{'connect_share'} - $user->{'join_share'});
+
+  my $welcome = "\n" . ('-' x 70) . "\n";
+  $welcome .= "***===[ $user->{'name'} :: $perm :: $user->{'client'} ]===***\n***===[ Member for: $member_time :: Share delta: $share_delta ]===***\n";
+  $welcome .= '-' x 70;
+
+  my @login = (
+    {
+      param    => "message",
+      message  => $welcome,
+      user     => $user->{name},
+      fromuser   => '',
+      type     => 2,
+    },
+  );
+
+  push(@return, @login);
+
   return @return;
 }
 
