648fc7922e930060cc47f16d504e4f2b191cd33c
odchbot <odchbot@gmail.com>
2013-09-12 23:13:20 +1000

    Altered commands to be a tablified commands list to a standard list of commands. Also added in specificity for commands so users can get information about a single command with -commands <commandname>

--- commands/commands.pm
+++ commands/commands.pm
@@ -2,8 +2,6 @@ package commands;
 
 use strict;
 use warnings;
-use Text::SimpleTable::AutoWidth;
-use Text::Tabs;
 use Storable qw(thaw);
 use FindBin;
 use lib "$FindBin::Bin/..";
@@ -14,30 +12,43 @@ use DCBUser;
 sub main {
   my $command = shift;
   my $user = shift;
-  my $chat = shift;
-
+  my @chatarray = split(/\s+/, shift);
+  my $specific = @chatarray ? shift(@chatarray) : '';
   my @return = ();
-  # TODO potentially provide all info stored in the array if a user specifies a command
-  # eg -commands kick will give all the info (basically a YAML dump)
-  my @captions = ('Command', 'Description', 'Aliases');
-  my $t = Text::SimpleTable::AutoWidth->new( max_width => 140, captions => \@captions );
+
   my $message = "\n";
-  foreach my $commands (sort keys %{$DCBCommon::registry->{commands}}) {
-    my $command = $DCBCommon::registry->{commands}->{$commands};
-    if ($commands =~ $DCBCommon::registry->{commands}->{$commands}->{name}) {
-      if (user_access($user, $command->{permissions})) {
-        my $alias = '';
-        if ($command->{alias}) {
-          my $aliases = thaw($command->{alias});
-          foreach (@$$aliases) {
-            $alias .= "$_ ";
-          }
+
+  if ($specific && $DCBCommon::registry->{commands}->{$specific} && (user_access($user, $DCBCommon::registry->{commands}->{$specific}->{permissions}))) {
+    my $command = $DCBCommon::registry->{commands}->{$specific};
+    $message .= DCBCommon::common_escape_string("$DCBSettings::config->{cp}") . "$command->{name}: $command->{description}";
+    if ($command->{alias}) {
+      $message .= "\nAliases: ";
+      my $aliases = thaw($command->{alias});
+      foreach (@$$aliases) {
+        $message .= "$_ ";
+      }
+    }
+    if ($command->{hooks}) {
+      $message .= "\nHooks: ";
+      my $hooks = thaw($command->{hooks});
+      foreach (@$$hooks) {
+        $message .= "$_ ";
+      }
+    }
+    $message .= "\nRequired: " . $command->{required};
+    $message .= "\nStatus: " . $command->{status};
+  }
+  else {
+    foreach my $commands (sort keys %{$DCBCommon::registry->{commands}}) {
+      my $command = $DCBCommon::registry->{commands}->{$commands};
+      if ($commands =~ $DCBCommon::registry->{commands}->{$commands}->{name}) {
+        if (user_access($user, $command->{permissions})) {
+          $message .= DCBCommon::common_escape_string("$DCBSettings::config->{cp}") . "$command->{name}: $command->{description}\n";
         }
-      $t->row(DCBCommon::common_escape_string("$DCBSettings::config->{cp}") . $command->{name}, $command->{description}, $alias);
       }
     }
   }
-  $message .= $t->draw();
+
   @return = (
     {
       param    => "message",
